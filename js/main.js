f769c4ad6d927289083cd9598b0b42e0ea2737d0
OG <olgageletina@gmail.com>
2021-09-23 19:25:22 -0400

    refiment

--- js/main.js
+++ js/main.js
@@ -3,9 +3,7 @@ const menuContent = document.getElementById("myNav");
 const hamburgerElems = document.getElementsByClassName("burger-line");
 const bodyTag = document.querySelector("body");
 const headerTextLines = document.getElementsByClassName("line-container");
-let thing = null;
-let j=1;
-let i=0;
+const year = new Date().getFullYear();
 
 // use barba CSS for page transitions empty hook are overwritten by CSS logic
 barba.use(barbaCss);
@@ -34,25 +32,14 @@ barba.hooks.beforeEnter(function (data) {
         });
   }
 
-  if (data.next.namespace === "light about") {
-    // slideAboutHeadline();
-    // console.log(slideAboutHeadline())
-
-    // if(j === elements.length - 1)
-    // {
-    //   clearInterval(slideAboutHeadline());
-    // }
-
-    // const sloganUlines = document.getElementsByClassName("caption-line");
-    //     Array.prototype.forEach.call(sloganUlines, function (elem) {
-    //       elem.classList.add("initialized");
-    //     }); 
-      }
-
   window.scrollTo({
     top: 0,
     behavior: "smooth",
   });
+
+  ga('set', 'page', window.location.pathname);
+  ga('send', 'pageview');
+
 });
 
 barba.init({
@@ -97,13 +84,16 @@ barba.init({
       },
       leave() {},
       enter() {},
-      // beforeEnter() {
-      //   slideAboutHeadline();
-      // }
     },
   ],
   // debug: true,
 });
+
+// update year
+document.addEventListener("DOMContentLoaded", function(event) 
+{
+  document.getElementById("year").innerHTML = year;
+})
 
 function toggleHeader(headerLines) {
   if (!headerLines[0].classList.contains("active")) {
@@ -143,58 +133,5 @@ function toggleNav() {
       elem.classList.remove("active");
       elem.classList.add("inactive");
     });
-  }
-}
-
-
-// function slideAboutHeadline() {
-// setInterval(function(){
-//   let elements = document.querySelectorAll('.about-line');
-  
-//   if (elements[i].classList.contains("active")) {
-//       elements[i].classList.add("inactive")
-//       elements[i].classList.remove("active")
-//       elements[i].classList.remove("inactive")
-//       elements[j].classList.add("active")
-//   }
-  
-//   if(j === elements.length - 1)
-//     {
-//         clearInterval(this);
-//     }
-
-//   j === elements.length - 1 ? j = 0 : j++; 
-//   i === elements.length - 1 ? i = 0 : i++; 
-  
-  
-//   // i++;
-//   console.log('j', j);
-//   console.log('i', i);
-
-//   // elements[i].classList.add("inactive")
-//   // elements[i].classList.remove("active")
-//   // elements[j].classList.add("active")
-
-// },1000);
-// }
-
-function slideAboutHeadline() {
-  let elements = document.querySelectorAll('.about-line');
-  // clearInterval(thing);
-  thing = setInterval(frame, 700);
-  function frame() {
-    if (j === 0) {
-      clearInterval(thing);
-    } else {
-      if (elements[i].classList.contains("active")) {
-        elements[i].classList.add("inactive")
-        elements[i].classList.remove("active")
-        elements[i].classList.remove("inactive")
-        elements[j].classList.add("active")
-      }
-      j === elements.length - 1 ? j = 0 : j++; 
-      i === elements.length - 1 ? i = 0 : i++; 
-   
-    }
   }
 }
